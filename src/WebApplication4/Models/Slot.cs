521ac7c0a80a75f88befde88807e57e8287ec9c4
Harika Naidu <nagaharika.n@gmail.com>
2019-02-27 15:53:56 -0600

    updated Student.cs and created Slot.cs

--- src/WebApplication4/Models/Slot.cs
+++ src/WebApplication4/Models/Slot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication4.Models
+namespace WebApplicationMVC.Models
 {
     public class Slot
     {
@@ -12,6 +12,5 @@ namespace WebApplication4.Models
         public int Term;
         public int CreditID;
         public int Status;
-
     }
 }
---
+++ src/WebApplication4/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Family { get; set; }
+        public string Given { get;  set; }
+        public int Snumber { get; set; }
+        public int Number_919 { get; set; }
+        
+    }
+}
