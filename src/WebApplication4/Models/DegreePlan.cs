f290895a2fe69a07b688b17e75582eb7b020228a
Sonam Gadekari <S533490@nwmissouri.edu>
2019-02-27 14:47:45 -0600

    updated models

---
+++ src/WebApplication4/Models/DegreePlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC.Models
+{
+    public class DegreePlan
+    {
+        public int DegreePlanID;
+        public int StudentID;
+        public int DegreePlanAbbrev;
+        public int DegreePlanName;
+
+    }
+}
---
+++ src/WebApplication4/Models/StudentTerm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC.Models
+{
+    public class StudentTerm
+    {
+        public int StudentTermID;
+        public int StudentID;
+        public int Term;
+        public int TermAbbrev;
+        public int TermName;
+    }
+}
