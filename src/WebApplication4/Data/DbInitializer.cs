173c0c75fc57ad69dc904d2fe96c8316a4f842be
Harika Naidu <nagaharika.n@gmail.com>
2019-03-02 22:08:52 -0600

    Added DbInitializer.cs with my Issues Assigned

--- src/WebApplication4/Data/DbInitializer.cs
+++ src/WebApplication4/Data/DbInitializer.cs
@@ -74,25 +74,68 @@ namespace WebApplication4.Data
 
             //This section ends: Credit
             //This section is for Harika: Degree Credit
-            if (context.Degrees.Any())
+            if (context.DegreeCredits.Any())
             {
-                Console.WriteLine("Degree Already exists:");
+                Console.WriteLine("Degree Credit Already exists:");
             }
             else
             {
-                var degrees = new Degree[]
+                var degreesCredits = new DegreeCredit[]
             {
-                new Degree{DegreeID = 1, DegreeAbbrev = "ACS+2", DegreeName ="MS ACS+2", NumberOfTerms = 5},
-                new Degree{DegreeID = 2, DegreeAbbrev = "ACS+DB", DegreeName ="MS ACS + DB", NumberOfTerms = 5},
-                new Degree{DegreeID = 3, DegreeAbbrev = "ACS+NF", DegreeName ="MS ACS + NF", NumberOfTerms = 5},
-                new Degree{DegreeID = 4, DegreeAbbrev = "ACS", DegreeName ="MS ACS", NumberOfTerms = 5},
-
+                new DegreeCredit{DegreeCreditID = 1 , DegreeID = 1 ,CreditID =460},
+                new DegreeCredit{DegreeCreditID = 2 , DegreeID = 1 ,CreditID =356},
+                new DegreeCredit{DegreeCreditID = 3 , DegreeID = 1 ,CreditID =542},
+                new DegreeCredit{DegreeCreditID = 4 , DegreeID = 1 ,CreditID =563},
+                new DegreeCredit{DegreeCreditID = 5 , DegreeID = 1 ,CreditID =560},
+                new DegreeCredit{DegreeCreditID = 6 , DegreeID = 1 ,CreditID =664},
+                new DegreeCredit{DegreeCreditID = 7 , DegreeID = 1 ,CreditID =618},
+                new DegreeCredit{DegreeCreditID = 8 , DegreeID = 1 ,CreditID =555},
+                new DegreeCredit{DegreeCreditID = 9 , DegreeID = 1 ,CreditID =691},
+                new DegreeCredit{DegreeCreditID = 10 , DegreeID = 1 ,CreditID =692},
+                new DegreeCredit{DegreeCreditID = 11 , DegreeID = 1 ,CreditID =6},
+                new DegreeCredit{DegreeCreditID = 12 , DegreeID = 1 ,CreditID =1},
+                new DegreeCredit{DegreeCreditID = 13 , DegreeID = 1 ,CreditID =2},
+                new DegreeCredit{DegreeCreditID = 14 , DegreeID = 2 ,CreditID =460},
+                new DegreeCredit{DegreeCreditID = 15 , DegreeID = 2 ,CreditID =542},
+                new DegreeCredit{DegreeCreditID = 16 , DegreeID = 2 ,CreditID =563},
+                new DegreeCredit{DegreeCreditID = 17 , DegreeID = 2 ,CreditID =560},
+                new DegreeCredit{DegreeCreditID = 18 , DegreeID = 2 ,CreditID =664},
+                new DegreeCredit{DegreeCreditID = 19 , DegreeID = 2 ,CreditID =618},
+                new DegreeCredit{DegreeCreditID = 20 , DegreeID = 2 ,CreditID =555},
+                new DegreeCredit{DegreeCreditID = 21 , DegreeID = 2 ,CreditID =691},
+                new DegreeCredit{DegreeCreditID = 22 , DegreeID = 2 ,CreditID =692},
+                new DegreeCredit{DegreeCreditID = 23 , DegreeID = 2 ,CreditID =6},
+                new DegreeCredit{DegreeCreditID = 24 , DegreeID = 2 ,CreditID =1},
+                new DegreeCredit{DegreeCreditID = 25 , DegreeID = 2 ,CreditID =2},
+                new DegreeCredit{DegreeCreditID = 26 , DegreeID = 3 ,CreditID =356},
+                new DegreeCredit{DegreeCreditID = 27 , DegreeID = 3 ,CreditID =542},
+                new DegreeCredit{DegreeCreditID = 28 , DegreeID = 3 ,CreditID =563},
+                new DegreeCredit{DegreeCreditID = 29 , DegreeID = 3 ,CreditID =560},
+                new DegreeCredit{DegreeCreditID = 30 , DegreeID = 3 ,CreditID =664},
+                new DegreeCredit{DegreeCreditID = 31 , DegreeID = 3 ,CreditID =618},
+                new DegreeCredit{DegreeCreditID = 32 , DegreeID = 3 ,CreditID =555},
+                new DegreeCredit{DegreeCreditID = 33 , DegreeID = 3 ,CreditID =691},
+                new DegreeCredit{DegreeCreditID = 34 , DegreeID = 3 ,CreditID =692},
+                new DegreeCredit{DegreeCreditID = 35 , DegreeID = 3 ,CreditID =6},
+                new DegreeCredit{DegreeCreditID = 36 , DegreeID = 3 ,CreditID =1},
+                new DegreeCredit{DegreeCreditID = 37 , DegreeID = 3 ,CreditID =2},
+                new DegreeCredit{DegreeCreditID = 38 , DegreeID = 4 ,CreditID =542},
+                new DegreeCredit{DegreeCreditID = 39 , DegreeID = 4 ,CreditID =563},
+                new DegreeCredit{DegreeCreditID = 40 , DegreeID = 4 ,CreditID =560},
+                new DegreeCredit{DegreeCreditID = 41 , DegreeID = 4 ,CreditID =664},
+                new DegreeCredit{DegreeCreditID = 42 , DegreeID = 4 ,CreditID =618},
+                new DegreeCredit{DegreeCreditID = 43 , DegreeID = 4 ,CreditID =555},
+                new DegreeCredit{DegreeCreditID = 44 , DegreeID = 4 ,CreditID =691},
+                new DegreeCredit{DegreeCreditID = 45 , DegreeID = 4 ,CreditID =692},
+                new DegreeCredit{DegreeCreditID = 46 , DegreeID = 4 ,CreditID =6},
+                new DegreeCredit{DegreeCreditID = 47 , DegreeID = 4 ,CreditID =1},
+                new DegreeCredit{DegreeCreditID = 48 , DegreeID = 4 ,CreditID =2},
            };
-                Console.WriteLine($"Inserted {degrees.Length} new degrees.");
+                Console.WriteLine($"Inserted {degreesCredits.Length} new degreesCredits.");
 
-                foreach (Degree d in degrees)
+                foreach(DegreeCredit d in degreesCredits)
                 {
-                    context.Degrees.Add(d);
+                    context.DegreeCredits.Add(d);
                 }
                 context.SaveChanges();
             }
@@ -147,25 +190,25 @@ namespace WebApplication4.Data
             }
             //This section ends
             //This section is for Sonam: Student.cs
-            if (context.Degrees.Any())
+            if (context.Students.Any())
             {
-                Console.WriteLine("Degree Already exists:");
+                Console.WriteLine("Student Already exists:");
             }
             else
             {
-                var degrees = new Degree[]
+                var students = new Student[]
             {
-                new Degree{DegreeID = 1, DegreeAbbrev = "ACS+2", DegreeName ="MS ACS+2", NumberOfTerms = 5},
-                new Degree{DegreeID = 2, DegreeAbbrev = "ACS+DB", DegreeName ="MS ACS + DB", NumberOfTerms = 5},
-                new Degree{DegreeID = 3, DegreeAbbrev = "ACS+NF", DegreeName ="MS ACS + NF", NumberOfTerms = 5},
-                new Degree{DegreeID = 4, DegreeAbbrev = "ACS", DegreeName ="MS ACS", NumberOfTerms = 5},
+                new Student{StudentID = 533726 , Family = "Narne" , Given ="Sai Tejaswini", Snumber =533726 , number_919=919571565},
+                new Student{StudentID = 533490 , Family = "Gadekari" , Given ="Sonam", Snumber =533490 , number_919=919568899},
+                new Student{StudentID = 533711 , Family = "Naidu" , Given ="Harika", Snumber =533711 , number_919=919570594},
+
 
            };
-                Console.WriteLine($"Inserted {degrees.Length} new degrees.");
+                Console.WriteLine($"Inserted {students.Length} new degrees.");
 
-                foreach (Degree d in degrees)
+                foreach (Student d in students)
                 {
-                    context.Degrees.Add(d);
+                    context.Students.Add(d);
                 }
                 context.SaveChanges();
             }
