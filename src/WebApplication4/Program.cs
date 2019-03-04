971604f51b515c638360d5fa6e24310af02ece68
Harika Naidu <nagaharika.n@gmail.com>
2019-03-04 15:58:05 -0600

    committed Program.cs

--- src/WebApplication4/Program.cs
+++ src/WebApplication4/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApplication4.Data;
 
 namespace WebApplication4
 {
@@ -14,11 +17,30 @@ namespace WebApplication4
     {
         public static void Main(string[] args)
         {
-           CreateWebHostBuilder(args).Build().Run();
+
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error");
+                }
+            }
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+       
+
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().Build();
     }
 }
